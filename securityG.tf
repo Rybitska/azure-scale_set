9f9f784dc5e81ab3bf6dce4470f955b32c2a6c97
mMeydan52 <nizamimeydan@gmail.com>
2022-06-12 21:34:26 +0000

    db_nsg created

---
+++ securityG.tf
@@ -0,0 +1,92 @@
+//////////////// Sec Group //////////////
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = azurerm_resource_group.terraform1.location
+  resource_group_name = azurerm_resource_group.terraform1.name
+
+  # We are creating a rule to allow traffic on port 80
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "MYSQL"
+    priority                   = 1200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [
+    azurerm_network_security_group.app_nsg
+  ]
+}
+
+
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "db-nsg"
+  location            = azurerm_resource_group.terraform1.location
+  resource_group_name = azurerm_resource_group.terraform1.name
+
+  # We are creating a rule to allow traffic on port 1433 for MYSQL
+  security_rule {
+    name                       = "Allow_MYSQL"
+    priority                   = 600
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "AnyOut"
+    priority                   = 121
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Any"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "db_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [
+    azurerm_network_security_group.app_nsg
+  ]
+}
