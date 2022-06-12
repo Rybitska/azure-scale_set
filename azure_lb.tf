314e47959f58294d1588ac6da79c3b54f0beec3a
Khrystyna Rybitska <krybitska@gmail.com>
2022-06-12 16:43:28 +0000

    added

--- azure_lb.tf
+++ azure_lb.tf
@@ -1,12 +1,12 @@
 
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "PublicIPForLB" {
   name                = "PublicIPForLB"
   location            = azurerm_resource_group.terraform1.location
   resource_group_name = azurerm_resource_group.terraform1.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_lb" "example" {
+resource "azurerm_lb" "TestLoadBalancer" {
   name                = "TestLoadBalancer"
   location            = azurerm_resource_group.terraform1.location
   resource_group_name = azurerm_resource_group.terraform1.name
