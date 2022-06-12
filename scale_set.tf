1f34f659f2d3094357df2faedc9c841e0f55b802
mMeydan52 <nizamimeydan@gmail.com>
2022-06-12 14:50:24 +0000

    lb hided

--- scale_set.tf
+++ scale_set.tf
@@ -62,3 +62,45 @@ resource "azurerm_linux_virtual_machine_scale_set" "ss" {
   }
 }
 #need target group
+
+//////////////// Sec Group //////////////
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = azurerm_resource_group.terraform1.location
+  resource_group_name = azurerm_resource_group.terraform1.name
+
+# We are creating a rule to allow traffic on port 80
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # We are creating a rule to allow traffic on port 22
+  security_rule {
+    name                       = "Allow_SSH"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [
+    azurerm_network_security_group.app_nsg
+  ]
+}
