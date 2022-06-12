cebfbcd0cbed3b73529e73854e8d736df4cc1d33
mMeydan52 <nizamimeydan@gmail.com>
2022-06-12 20:16:20 +0000

    lb port 22 opened

--- azure_lb.tf
+++ azure_lb.tf
@@ -49,12 +49,37 @@ resource "azurerm_lb_rule" "RuleA" {
   ]
 }
 
+resource "azurerm_lb_rule" "RuleB" {
+ # resource_group_name            = azurerm_resource_group.terraform1.name
+  loadbalancer_id                = azurerm_lb.wp_LoadBalancer.id
+  name                           = "RuleB"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.scalesetpool.id]
+  depends_on = [
+    azurerm_lb.wp_LoadBalancer
+  ]
+}
+
 // Here we are defining the Health Probe
 resource "azurerm_lb_probe" "ProbeA" {
  # resource_group_name = azurerm_resource_group
   loadbalancer_id     = azurerm_lb.wp_LoadBalancer.id
   name                = "probeA"
   port                = 80
+  protocol            = "Tcp"
+  depends_on = [
+    azurerm_lb.wp_LoadBalancer
+  ]
+}
+
+resource "azurerm_lb_probe" "ProbeB" {
+ # resource_group_name = azurerm_resource_group
+  loadbalancer_id     = azurerm_lb.wp_LoadBalancer.id
+  name                = "probeB"
+  port                = 22
   protocol            = "Tcp"
   depends_on = [
     azurerm_lb.wp_LoadBalancer
